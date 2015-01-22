3f4b3877278b00a3240999853d26841c99c4fede
fubuki <fubuki@users.noreply.github.com>
2015-01-22 18:14:06 +0800

    Update rpc.erl

--- rpc.erl
+++ rpc.erl
@@ -1,8 +1,9 @@
 %一個簡單的 rpc 程式
 -module(rpc).
+-export([call/0]).
 
 
-
-call(Node, Module, Func, Args) ->
-
-        end.
+call() ->
+    Pid = self(),
+    Node = node(Pid),
+    rpc:call(Node, erlang, is_process_alive, [Pid]).
